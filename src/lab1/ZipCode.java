0d15ab084b091b47a89481cb2bef89252d211e6c
2476382 <2476382@host.docker.internal>
2025-08-27 10:33:51 -0400

    Implement basic ZipCode class

--- src/lab1/ZipCode.java
+++ src/lab1/ZipCode.java
@@ -5,5 +5,29 @@ package lab1;
  * @author 2476382
  */
 public class ZipCode {
-    
+
+    private Integer Zip = null;
+    private String barCode;
+
+    public ZipCode(int code) {
+        if (String.valueOf(code).length() > 5) {
+            System.out.println("The entered integer zip code has more than 5 digits");
+        } else {
+            this.Zip = code;
+
+        }
+    }
+
+    public ZipCode(String barCode) {
+        this.barCode = barCode;
+    }
+
+    public String GetBarCode() {
+        if (this.Zip == null) {
+            System.out.println("The zip is invalid (more than 5 digits)");
+        }
+        
+        return barCode;
+    }
+
 }
